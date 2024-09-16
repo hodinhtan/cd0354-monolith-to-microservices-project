8e6f197f77653a0f38571892db3fbec3a8569e67
tan <hodinhtan@gmail.com>
2024-09-16 10:45:30 +0700

    lam bai tap

---
+++ set_env.bat
@@ -0,0 +1,11 @@
+:: This file is used for convenience of local development.
+:: DO NOT STORE YOUR CREDENTIALS INTO GIT
+set POSTGRES_USERNAME="postgres"
+set POSTGRES_PASSWORD="1"
+set POSTGRES_HOST="localhost"
+set POSTGRES_DB="postgres"
+@REM set AWS_BUCKET=arn:aws:s3:::mybucket1200798
+@REM set AWS_REGION=us-east-1
+@REM set AWS_PROFILE=default
+set JWT_SECRET="testing"
+set URL="http://localhost:8100"
